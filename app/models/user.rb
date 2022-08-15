50d497dffe52ad7c4ead9df343f3d5a14d384995
RomiCLT <cuidemoslatierrachile@gmail.com>
2022-08-14 22:43:45 -0400

    constructora

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-enum role: [:user, :encargado,:administrador]
+enum rol: [:usuario, :encargado,:administrador]
 after_initialize :set_default_role, :if => :new_record?
 def set_default_role
   self.role ||= :user
